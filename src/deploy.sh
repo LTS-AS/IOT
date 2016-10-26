33548703ca09a6c2e50dd74f8ef6b886bd4278a3
Havard <havard.line@gmail.com>
2016-10-26 15:13:29 +0200

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,3 +7,6 @@ sudo apt-get install vim \
 
 echo "Generating SSH-keys for "$device_name
 ssh-keygen -t ed25519 -C $device_name -f ~/.ssh/id_ed25519 -q -N ""
+
+echo "Moving list of authorized SSH-users"
+mv ~/IOT/config/authorized_hosts ~/.ssh/authorized_hosts
