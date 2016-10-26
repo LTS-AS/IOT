c505db77334dbe6b82736885cb20b33f1a779974
Havard <havard.line@gmail.com>
2016-10-26 15:09:32 +0200

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-. ../config/init.sh
+. ~/IOT/config/init.sh
 
 sudo apt-get install vim \
   git \
   --assume-yes
 
 echo "Generating SSH-keys for "$device_name
-ssh-keygen -t ed25519 -C $device_name -f /tmp/sshkey -q -N ""
+ssh-keygen -t ed25519 -C $device_name -f ~/.ssh/id_ed25519 -q -N ""
