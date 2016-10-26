d20f88a365c41435d1c296c7b6f39824611557af
Havard <havard.line@gmail.com>
2016-10-26 14:49:44 +0200

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,5 +5,5 @@ sudo apt-get install vim \
   git \
   --assume-yes
 
-echo $device_name
-#ssh-keygen -t Curve25519 -C $device_name
+echo "Generating SSH-keys for "$device_name
+ssh-keygen -t Curve25519 -C $device_name
