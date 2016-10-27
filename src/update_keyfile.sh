3440679a90486376a84ee898f2fc94e6b35a4800
Havard <havard.line@gmail.com>
2016-10-27 11:44:06 +0200

    Update update_keyfile.sh

--- src/update_keyfile.sh
+++ src/update_keyfile.sh
@@ -1,3 +1,4 @@
+#This file does not belong here. I do not know where to put it. It updates the authorized_keys file from the key-management repo.
 git pull https://github.com/LTS-AS/IOT.git
 git remote add upstream https://github.com/LTS-AS/key-management.git
 git checkout upstream/master config/authorized_keys
