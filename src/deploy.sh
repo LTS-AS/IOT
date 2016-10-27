55c78b8648704a7d957d25fe478b826fef9d6efa
Havard <havard.line@gmail.com>
2016-10-27 11:33:44 +0200

    deploy.sh is OK for now

--- src/deploy.sh
+++ src/deploy.sh
@@ -15,8 +15,8 @@ sudo apt-get install \
 	vim \
 	--assume-yes
 
-#echo "Generating SSH-keys for "$device_name
-#ssh-keygen -t ed25519 -C $device_name -f ~/.ssh/id_ed25519 -q -N ""
+echo "Generating SSH-keys for "$device_name
+ssh-keygen -t ed25519 -C $device_name -f ~/.ssh/id_ed25519 -q -N ""
 
 echo "Moving list of authorized SSH-users"
 cp ~/IOT/config/authorized_keys ~/.ssh/authorized_keys
