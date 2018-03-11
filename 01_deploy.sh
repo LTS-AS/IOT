a962327f906b65db9eb28a8e58ece972046f4de1
Håvard B Line <havard.line@gmail.com>
2018-03-11 21:22:20 +0100

    Added code for hashing

--- 01_deploy.sh
+++ 01_deploy.sh
@@ -24,3 +24,4 @@ echo "Adding trusted SSH-keys"
 cp ./key-management/ssh/authorized_keys ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
+
--- conftest.py
+++ conftest.py
@@ -1,11 +1,23 @@
-import configparser
+import configparser, hashlib
 
 config = configparser.ConfigParser()
 
-with open('/etc/os-release', 'r') as f:
-    config_string = u'[section]\n'+f.read()
+#with open('/etc/os-release', 'r') as f:
+#    config_string = u'[section]\n'+f.read()
 
-config.read_string(config_string)
-print(config['section']['ID'])
-print(config['section']['VERSION_ID'])
+#config.read_string(config_string)
+#print(config['section']['ID'])
+#print(config['section']['VERSION_ID'])
 
+def calculate_hash(file_path):
+    hasher = hashlib.sha256()
+    BLOCKSIZE = 65536
+    with open(file_path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+if __name__ == "__main__":
+    print(calculate_hash("C:\\hei-hei.txt"))
